f249aa42ff5c8c6d126ea94e18c73998bdf9ab2f
Saad Shamsi <114074040+saad-shamsi@users.noreply.github.com>
2024-02-12 00:20:05 +0500

    layout created

---
+++ app/(site)/page.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <div
+      className="bg-neutral-900 
+    rounded-lg
+    h-full
+    overflow-hidden
+    overflow-y-auto"
+    >
+      <Header>
+        <h1>Welcome to our website!</h1>
+      </Header>
+    </div>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+import Sidebar from "@/components/Sidebar";
+
+const figtree = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Spotify Clone",
+  description: "listen to the Quran",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={figtree.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
+    </html>
+  );
+}
